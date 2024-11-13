ede54ed97d73f4d9dc3c97f4304d5496f2c85301
siuupriyanshu <mandalpriyanshu22@gmail.com>
2024-11-13 17:48:20 +0545

    **

--- content.js
+++ content.js
@@ -1,13 +1,6 @@
-// Check the state of the extension and pause or play the video
-chrome.storage.sync.get("isEnabled", (data) => {
-  const isEnabled = data.isEnabled === false ? false : true;
-  const video = document.querySelector('video');
-
-  if (video) {
-    if (isEnabled) {
-      video.play(); // Play video if extension is enabled
-    } else {
-      video.pause(); // Pause video if extension is disabled
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    const video = document.querySelector("video");
+    if(request.action === "play" && video && video.paused) {
+        video.play();
     }
-  }
 });
